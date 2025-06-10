cba3df59a9c0d389cb33b3bd5910453781f33c75
willjbraga <williamjubraga@gmail.com>
2025-06-09 23:38:01 -0300

    Salvando arquivos da seção 5

---
+++ Exercicio Seção 5/Exercicio Seção 5/Conta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Seção_5 {
+    public class Conta {
+        public int Numero {  get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; set; }
+
+        public Conta(int numero, string titular, double saldo) {
+            this.Numero = numero;
+            this.Titular = titular;
+            this.Saldo = saldo;
+        }
+
+        public Conta(int numero, string titular) {
+            this.Numero = numero;
+            this.Titular = titular;
+            Saldo = 0;
+        }
+
+        public void Deposito(double valor) {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            Saldo -= valor + 5 ; 
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercicio Seção 5/Exercicio Seção 5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Seção_5 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int conta;
+            string titular;
+            char dep_inicial;
+            double saldo;
+
+            Console.Write("Entre o número da conta: ");
+            conta = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Entre o titular da conta: ");
+            titular = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Haverá depósito inicial (s/n)?");
+            dep_inicial = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Conta c = new Conta(conta, titular);
+
+            if (dep_inicial == 's') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+                c.Deposito(saldo);
+            }
+
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para depósito: ");
+            c.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para saque: ");
+            c.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
